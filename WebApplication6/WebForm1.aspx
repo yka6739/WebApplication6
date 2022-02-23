8bd0d928b4bd48f1fd585600e62c1516df422eab
anand.mani <anand.mani@6WSR022>
2022-02-23 14:55:41 +0530

    Add project files.

---
+++ WebApplication6/WebForm1.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication6.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+
+
+<%--<form>
+<div>
+<table>
+<tr>
+<td>
+Enter Username:
+</td>
+<td>
+    
+<asp:TextBox ID="txtusername" runat="server"></asp:TextBox>
+</td>
+</tr>
+<tr>
+<td>
+</td>
+<td>
+<asp:Button ID="btnSubmit" runat="server" Text="Submit" onclick="btnSubmit_Click" />
+</td>
+</tr>
+<tr>
+<td align="right">
+First Name:
+</td>
+<td>
+<asp:Label ID="lblfname" runat="server" Font-Bold="true"></asp:Label> 
+</td>
+</tr>
+<tr>
+<td align="right">
+Last Name:
+</td>
+<td>
+<asp:Label ID="lbllname" runat="server" Font-Bold="true" ></asp:Label> 
+</td>
+</tr>
+<tr>
+<td align="right">
+Email:
+</td>
+<td>
+<asp:Label ID="lblemail" runat="server" Font-Bold="true"></asp:Label> 
+</td>
+</tr>
+</table>
+</div>
+</form>--%>
+
+
+
+
+    <asp:GridView ID="GridView1" runat="server">
+
+    </asp:GridView>
+
+
+
+
+</asp:Content>
---
+++ WebApplication6/WebForm1.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.DirectoryServices;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication6
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+            }
+        }
+        private void BindGrid()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["ADConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT UserID,EmailID FROM cc"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            GridView1.DataSource = dt;
+                            GridView1.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        //protected void btnSubmit_Click(object sender, EventArgs e)
+        //{
+        //    string connection = ConfigurationManager.ConnectionStrings["ADConnection"].ToString();
+        //    DirectorySearcher dssearch = new DirectorySearcher(connection);
+        //   dssearch.Filter = "(sAMAccountName=" + txtusername.Text + ")";
+        //    SearchResult sresult = dssearch.FindOne();
+        //    DirectoryEntry dsresult = sresult.GetDirectoryEntry();
+        //    lblfname.Text = dsresult.Properties["Name"][0].ToString();
+        //    lbllname.Text = dsresult.Properties["LName"][1].ToString();
+        //    lblemail.Text = dsresult.Properties["Email"][2].ToString();
+        //}
+    }
+}
